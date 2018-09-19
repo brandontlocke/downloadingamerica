88ae00f5e71efbc7faa28a79318ccb97edbd12b9
Brandon <brandontlocke@gmail.com>
2018-09-19 11:00:02 -0400

    now automatically grabs all items

--- downloadingamerica.py
+++ downloadingamerica.py
@@ -3,13 +3,21 @@
 
 import json, re, urllib.request
 
+#request the url to retrieve
 chronam = input("What is the results URL you want to download?")
-results = input("How many results are there?")
 
-with urllib.request.urlopen(chronam+'&rows='+results+'&format=json') as json_file:  
+#by default, only 20 items are returned; this checks to see how many total items are there
+checkrows = urllib.request.urlopen(chronam+'&format=json')
+values = json.load(checkrows)
+rows = values['totalItems']
+checkrows.close()
+
+#re-requests the URL with the total number of results attached
+with urllib.request.urlopen(chronam+'&rows='+str(rows)+'&format=json') as json_file:
     data = json.loads(json_file.read().decode())
     for p in data['items']:
         filename = p['date']+'_'+p['title']+'_'+"pg"+p['page']
         file = open(re.sub('[^a-zA-Z0-9_]', '', filename)+".txt", "w")
         file.write(p['ocr_eng'])
+
 file.close()
