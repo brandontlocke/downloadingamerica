c07908954f232dea8f493afd8f6df575b10197a5
Brandon <brandontlocke@gmail.com>
2018-09-27 17:39:09 -0400

    update with readme

--- downloadingamerica.py
+++ downloadingamerica.py
@@ -1,9 +1,9 @@
 
 #!/usr/bin/env python
-import json, re, urllib.request, csv
+import sys, json, re, urllib.request, csv, datetime
 
 #request the url to retrieve
-chronam = input("What is the results URL you want to download?")
+chronam = input("Paste in the URL for the ChronAm search results you want to download: ")
 
 #by default, only 20 items are returned; this checks to see how many total items are there
 checkrows = urllib.request.urlopen(chronam+'&format=json')
@@ -16,12 +16,22 @@ with urllib.request.urlopen(chronam+'&rows='+str(rows)+'&format=json') as json_f
     data = json.loads(json_file.read().decode())
 json_file.close()
 
+#opens a metadata csv, writes a header, then for each item it creates a filename, prints the text, and prints the metadata to the csv
 with open('metadata.csv', 'w') as metadata:
     metawriter = csv.writer(metadata, delimiter=',')
-    metawriter.writerow(['file', 'title', 'date', 'pageNo', 'city', 'county', 'state', 'pageURL'])
+    metawriter.writerow(['file', 'title', 'date', 'page_num', 'city', 'county', 'state', 'page_url'])
     for p in data['items']:
         filename = re.sub('[^a-zA-Z0-9_]', '', p['date']+'_'+p['title']+'_'+"pg"+p['page'])
         file = open(filename + ".txt", "w")
         file.write(p['ocr_eng'])
         metawriter.writerow([filename, p['title'], p['date'], p['page'], p['city'][0], p['county'][0], p['state'][0], 'https://chroniclingamerica.loc.gov' + p['id']])
 metadata.close()
+
+#creates a readme with some very simple information about the corpus
+with open('readme.txt', 'w') as readme:
+    readme.write("Downloaded: " + str(datetime.datetime.now()) + '\n\n' + "Num of results: " + str(rows) + " newspaper pages" + '\n\n' + "Search URL: " + chronam + '\n\n' + "Search results in JSON: " + chronam + "&format=json&rows=" + str(rows) + "\n\n")
+    search = chronam.split("&")
+    state = search[0].split("?")
+    readme.write("===Search terms=== " + '\n' + state[1] + '\n' + search[1] + '\n' + search[2] + '\n' + search[3] + search[4] + '\n' + search[5] + '\n' + search[6] + '\n' + search[7] + '\n' + search[8] + '\n' + search[9] + ' & ' + search[10] + '\n\n')
+    readme.write("===Compiled with===\n📰📂DownloadingAmerica📂📰\nhttps://github.com/brandontlocke/downloadingamerica ")
+readme.close()
---
+++ readme.md
@@ -0,0 +1,27 @@
+# 📰📂DownloadingAmerica📂📰
+
+A Python script for bulk downloading individual plain text files from the [Library of Congress's Chronicling America](https://chroniclingamerica.loc.gov/).
+
+Given a Chronicling America search result, this will download the plain text of each page into its own text file, write some of the metadata to a csv file, and create a readme that explains the search.
+
+## Dependencies
+To use this, you'll need to have Python 3 installed, and access to some kind of command line interface [I'd recommend Terminal on OS X or GitBash on Windows].
+
+## Instructions
+Create a folder where you want your text files to live. [Your life will be better if this folder is empty.]
+
+Save the downloadingamerica.py file in that folder.
+
+Using a command line interface, navigate to the folder. [For a folder named 'chronam' on the Desktop, you would type `cd Desktop/chronam`]
+
+Run the script (type `python downloadingamerica.py` )
+
+It will ask for a URL. Paste in the regular results URL from any Chronicling America search result page and hit enter.
+
+## Notes
+This is very much in beta and has only been tested with a few different search scenarios. It's possible that extremely large results will break it.
+
+Some newspapers are missing key metadata (such as page numbers) which may result in files overwriting themselves. Be sure to check your results.
+
+### Next Steps
+I'm open to hearing feedback if there is anything people would like to have changed. At some point I would like to parse the search query to make a nicer looking readme, but that will wait another day.
