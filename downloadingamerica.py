9b5a962c2280c5eca5e39cc10315f80c784179d0
Brandon Locke <brandontlocke@gmail.com>
2020-10-12 11:52:48 -0700

    can handle empty metadata objects

--- downloadingamerica.py
+++ downloadingamerica.py
@@ -3,32 +3,28 @@
 import json, re, urllib.request, csv, datetime, time
 from socket import timeout
 
-import socket
-timeout = 1000000
-socket. setdefaulttimeout(timeout)
-
 #request the url to retrieve
 chronam = input("Paste in the URL for the ChronAm search results you want to download: ")
 
 #requests the URL in json with 20 rows
-with urllib.request.urlopen(chronam+'&format=json') as json_file:
+with urllib.request.urlopen(chronam+'&format=json&rows=10', timeout=200) as json_file:
     data = json.loads(json_file.read().decode('utf8'))
 
-#this creates a metadata csv, adds a header, grabs 20 items at a time from the API, cycles through each item and writes a row in metadata and creates a text file with the OCR. After it's done all 20, it goes back for the next 20.
+#this creates a metadata csv, adds a header, grabs 10 items at a time from the API, cycles through each item and writes a row in metadata and creates a text file with the OCR. After it's done all 20, it goes back for the next 20.
 
 #sets base variables to track the current page of json results & number of items with ocr_eng
 pg=1
 textcounter=0
 time.sleep(5)
-#creates a loop that continues until the final group of 20 json results have come in
+#creates a loop that continues until the final group of 10 json results have come in
 while data['endIndex'] < data['totalItems']:
     #creates metadata.csv file and writes a header the first time through
     with open('metadata.csv', 'a', encoding='utf8') as metadata:
         metawriter = csv.writer(metadata, delimiter=',')
         if pg==1:
             metawriter.writerow(['file', 'title', 'date', 'edition', 'page', 'sequence', 'city', 'county', 'state', 'page_url'])
-        #loads a set of 20 results from the api and puts them in the variable 'data'
-        with urllib.request.urlopen(chronam+'&rows=20&format=json&page=' + str(pg), timeout=100000) as json_file:
+        #loads a set of 10 results from the api and puts them in the variable 'data'
+        with urllib.request.urlopen(chronam+'&rows=10&format=json&page=' + str(pg), timeout=200) as json_file:
             data = json.loads(json_file.read().decode('utf8'))
         json_file.close()
 
@@ -50,7 +46,19 @@ while data['endIndex'] < data['totalItems']:
                 #count every file with ocr_eng so that it can be compared to the folder
                 textcounter+=1
             #write metadata to the csv
-            metawriter.writerow([filename, p['title'], p['date'], p['edition'], p['page'], p['sequence'], p['city'][0], p['county'][0], p['state'][0], 'https://chroniclingamerica.loc.gov' + p['id']])
+            if p.get(['city'][0]) == []:
+                city = ''
+            else:
+                city=p['city'][0]
+            if p.get(['county'][0]) == []:
+                county = ''
+            else:
+                county=p['county'][0]
+            if p.get(['state'][0]) == []:
+                state = ''
+            else:
+                state=p['state'][0]
+            metawriter.writerow([filename, p['title'], p['date'], p['edition'], p['page'], p['sequence'], city, county, state, 'https://chroniclingamerica.loc.gov' + p['id']])
     pg+=1
     time.sleep(5)
 
